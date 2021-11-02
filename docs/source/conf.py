c9870582748994da93b1c2386085e6ce401dedcb
Rozemberczki <kmdb028@astrazeneca.net>
2021-11-02 10:52:07 +0000

    Docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,9 +35,9 @@ html_theme_options = {
     'logo_only': True,
 }
 
-html_logo = '_static/img/rexmex_small.jpg'
-html_static_path = ['_static']
-html_context = {'css_files': ['_static/css/custom.css']}
+html_logo = 'static/img/rexmex_small.jpg'
+html_static_path = ['static']
+html_context = {'css_files': ['static/css/custom.css']}
 
 add_module_names = False
 

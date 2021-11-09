45f5b874c540377f17215002b479982e58edc5db
Rozemberczki <kmdb028@astrazeneca.net>
2021-11-09 10:23:36 +0000

    Integration Tests.

--- rexmex/metrics.py
+++ rexmex/metrics.py
@@ -1,24 +1,31 @@
 import pandas as pd
-from typing import List, Tuple
+from typing import List, Dict
+from abc import ABC, abstractmethod
 from sklearn.metrics  import roc_auc_score, f1_score
 
-class MetricSet(object):
+class MetricSet(ABC):
     """
     """
-    def _get_metrics(self, filter: List[str]=None) -> List[Tuple[str]]:
+    @abstractmethod
+    def setup_basic_metrics(self):
+        pass
+
+    def _get_metrics(self, filter: List[str]=None) -> Dict:
         if filter is None:
             selected_metrics = self._metrics
         else:
-            selected_metrics = [(name, metric) for name, metric in self._metrics]
+            selected_metrics = {name: metric for name, metric in self._metrics.items()}
         return selected_metrics
 
-    def add_new_metrics(self, metrics: List[Tuple]):
-        self._metrics = self._metrics + metrics
-        return self._metrics
-
+    #def add_new_metrics(self, metrics: List[Tuple]):
+    #    self._metrics = self._metrics + metrics
+    #    return self._metrics
+    def reset_metrics(self):
+        self._metrics = {}
+    
     def get_performance_metrics(self, y_true, y_score, filter: List[str]=None) -> pd.DataFrame:
         selected_metrics = self._get_metrics(filter)
-        performance_metrics = {name: [metric(y_true, y_score)] for name, metric in selected_metrics}
+        performance_metrics = {name: [metric(y_true, y_score)] for name, metric in selected_metrics.items()}
         performance_metrics = pd.DataFrame.from_dict(performance_metrics)
         return performance_metrics
 
@@ -27,18 +34,34 @@ class ClassificationMetricSet(MetricSet):
     """
     def __init__(self, cutoff: float=0.5):
         self.cutoff = cutoff
-        self._metrics = []
+        self._metrics = {}
 
     def setup_basic_metrics(self):
-        self._metrics.append(("roc_auc", roc_auc_score))
-        self._metrics.append(("f1_score", roc_auc_score))
+        self._metrics["roc_auc"] = roc_auc_score
+        self._metrics["f1_score"] = roc_auc_score
 
 
 class RankingMetricSet(MetricSet):
-    pass
+    
+
+    def __init__(self):
+        self._metrics = {}
+
+    def setup_basic_metrics(self):
+        pass
+
 
 class RatingMetricSet(MetricSet):
-    pass
+    def __init__(self):
+        self._metrics = {}
+
+    def setup_basic_metrics(self):
+        pass
+
 
 class CoverageMetricSet(MetricSet):
-    pass
+    def __init__(self):
+        self._metrics = {}
+
+    def setup_basic_metrics(self):
+        pass

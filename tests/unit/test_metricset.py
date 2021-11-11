41c89c1f00232b67a3e9461fede39646530d144b
Rozemberczki <kmdb028@astrazeneca.net>
2021-11-11 14:30:25 +0100

    Rating metric docs.

--- tests/unit/test_metricset.py
+++ tests/unit/test_metricset.py
@@ -7,8 +7,6 @@ from rexmex.metricset import ClassificationMetricSet
 class TestMetrics(unittest.TestCase):
 
     def test_classification_metric_set(self):
-        scores = pd.DataFrame([[1, 0.1], [0,0.5], [1,0.99]], columns = ["y_true", "y_score"])
         metric_set = ClassificationMetricSet()
-        performance_metrics = metric_set.get_performance_metrics(scores.y_true, scores.y_score)
-        assert performance_metrics.shape == (1, 6)
+        assert "auc_score" in metric_set
 

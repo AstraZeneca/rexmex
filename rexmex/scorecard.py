ec14eb4611b3af6163e35138ad074f0b4ef7aa8b
Rozemberczki <kmdb028@astrazeneca.net>
2021-11-11 14:35:50 +0100

    Rating metric docs.

--- rexmex/scorecard.py
+++ rexmex/scorecard.py
@@ -7,15 +7,15 @@ class ScoreCard(object):
         
         self.metric_set = metric_set
 
-    def get_performance_metrics(self, y_true, y_score) -> pd.DataFrame:
+    def _get_performance_metrics(self, y_true, y_score) -> pd.DataFrame:
         """
         """
         performance_metrics = {name: [metric(y_true, y_score)] for name, metric in self.metric_set.items()}
         performance_metrics = pd.DataFrame.from_dict(performance_metrics)
         return performance_metrics
 
-    def group_calculate(self, scores: pd.DataFrame, groupping: List[str]=None) -> pd.DataFrame:
+    def calculate(self, scores: pd.DataFrame, groupping: List[str]=None) -> pd.DataFrame:
         if groupping is not None:
              scores = scores.groupby(groupping)
-        performance_metrics = scores.apply(lambda group: self.get_performance_metrics(group.y_true, group.y_score))
+        performance_metrics = scores.apply(lambda group: self._get_performance_metrics(group.y_true, group.y_score))
         return performance_metrics

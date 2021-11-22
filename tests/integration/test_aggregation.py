e972a0a7506f668ffefb67ae7a6b4f4827e7175c
Rozemberczki <kmdb028@astrazeneca.net>
2021-11-22 11:33:24 +0000

    Base class repr and print

--- tests/integration/test_aggregation.py
+++ tests/integration/test_aggregation.py
@@ -3,7 +3,7 @@ import unittest
 
 from rexmex.scorecard import ScoreCard
 from rexmex.dataset import DatasetReader
-from rexmex.metricset import ClassificationMetricSet, RegressionMetricSet
+from rexmex.metricset import ClassificationMetricSet, RatingMetricSet
 
 class TestMetricAggregation(unittest.TestCase):
 
@@ -18,7 +18,7 @@ class TestMetricAggregation(unittest.TestCase):
     def test_regression(self):
         reader = DatasetReader()
         scores = reader.read_dataset()
-        metric_set = RegressionMetricSet()
+        metric_set = RatingMetricSet()
         score_card = ScoreCard(metric_set)
         performance_metrics = score_card.generate_report(scores, groupping=["source_group"])
         assert performance_metrics.shape == (10, 7)

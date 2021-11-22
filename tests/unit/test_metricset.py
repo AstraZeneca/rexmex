86cc5f7dbae5b8a1a64d9e286d3008719b0210b5
Rozemberczki <kmdb028@astrazeneca.net>
2021-11-22 13:39:17 +0000

    License Badge

--- tests/unit/test_metricset.py
+++ tests/unit/test_metricset.py
@@ -1,7 +1,7 @@
 import unittest
 import pytest
 import pandas as pd
-
+from sklearn_metrics import accuracy_score
 from rexmex.metricset import ClassificationMetricSet, RatingMetricSet, RankingMetricSet, CoverageMetricSet
 
 class TestMetrics(unittest.TestCase):
@@ -13,10 +13,15 @@ class TestMetrics(unittest.TestCase):
         assert repr(RatingMetricSet()) == "RatingMetricSet()"
 
     def test_metric_filter(self):
-        assert 2 == 2
+        metric_set = ClassificationMetricSet()
+        metric_set.filter_metric_set(["roc_auc", "pr_auc"])
+        assert len(metric_set) == 2
+        metric_set = ClassificationMetricSet()
+        metric_set.filter_metric_set(["fake_metric"])
+        assert len(metric_set) == 0
 
     def test_metric_add(self):
-        assert 2 == 2
+        assert 
 
     def test_print(self):
         assert 2 == 2

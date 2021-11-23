81f06173d593f929da51ef2675c052f72693756d
Rozemberczki <kmdb028@astrazeneca.net>
2021-11-23 11:10:09 +0000

    New Metrics

--- tests/unit/test_metrics.py
+++ tests/unit/test_metrics.py
@@ -4,6 +4,7 @@ import unittest
 from sklearn.metrics import precision_score, recall_score
 
 class TestMetrics(unittest.TestCase):
+    def __init__(self):
         self.x = 2
 
     def test_representation(self):

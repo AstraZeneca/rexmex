a15540864e1da8641f4d9270069bcb1a023f0910
Rozemberczki <kmdb028@astrazeneca.net>
2021-11-23 13:11:35 +0000

    New Metrics

--- tests/unit/test_metrics.py
+++ tests/unit/test_metrics.py
@@ -1,12 +1,25 @@
 import pytest
 import unittest
+import numpy as np
 
 from sklearn.metrics import precision_score, recall_score
+from rexmex.metrics import condition_positive, condition_negative
 
-class TestMetrics(unittest.TestCase):
+class TestClassificationMetrics(unittest.TestCase):
+    """
+    Newly defined metric tests.
+    """
 
     def setUp(self):
-        self.x = 2
+        self.y_true = np.array([0, 1, 1, 1, 1, 0, 0, 0, 0])
+        self.y_scores = np.array([0.7, 0.99, 1.0, 0.2, 0.3, 0.6, 0.1, 0.3, 0.0])
 
-    def test_representation(self):
-        assert self.x == 2
+    def test_conditions(self):
+        assert condition_positive(self.y_true) == 4
+        assert condition_negative(self.y_true) == 5
+
+    def test_false_and_true(self):
+        assert false_positive(self.y_true, self.y_scores) == 2
+        assert false_negative(self.y_true, self.y_scores) == 2 
+        assert true_positive(self.y_true, self.y_scores) == 2 
+        assert true_negative(self.y_true, self.y_scores) == 3

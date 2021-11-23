f85ef60b103622ce40f889011ef7fe765d4b39c2
Rozemberczki <kmdb028@astrazeneca.net>
2021-11-23 13:15:53 +0000

    New Metrics

--- tests/unit/test_metrics.py
+++ tests/unit/test_metrics.py
@@ -7,23 +7,23 @@ from rexmex.metrics import condition_positive, condition_negative
 
 class TestClassificationMetrics(unittest.TestCase):
     """
-    Newly defined metric tests.
+    Newly defined classification metric tests.
     """
     def setUp(self):
         self.y_true = np.array([0, 1, 1, 1, 1, 0, 0, 0, 0])
-        self.y_scores = np.array([0.7, 0.99, 1.0, 0.2, 0.3, 0.6, 0.1, 0.3, 0.0])
+        self.y_scores = np.array([1, 1, 1, 0, 0, 1, 0, 0, 0])
 
     def test_conditions(self):
         assert condition_positive(self.y_true) == 4
         assert condition_negative(self.y_true) == 5
 
     def test_false_and_true(self):
-        assert false_positive(self.y_true, self.y_scores) == 2
-        assert false_negative(self.y_true, self.y_scores) == 2 
-        assert true_positive(self.y_true, self.y_scores) == 2 
-        assert true_negative(self.y_true, self.y_scores) == 3
+        assert binarize(false_positive(self.y_true, self.y_scores)) == 2
+        assert binarize(false_negative(self.y_true, self.y_scores)) == 2 
+        assert binarize(true_positive(self.y_true, self.y_scores)) == 2 
+        assert binarize(true_negative(self.y_true, self.y_scores)) == 3
 
-class TestRatingMetrics(unittest.TestCase)
+class TestRatingMetrics(unittest.TestCase):
 
     def setUp(self):
         pass

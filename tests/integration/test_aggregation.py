44c3895e14cfc2acf3ca0b2890adb15b0a7590ed
Rozemberczki <kmdb028@astrazeneca.net>
2021-11-25 15:18:15 +0000

    Simplify tests with setup.

--- tests/integration/test_aggregation.py
+++ tests/integration/test_aggregation.py
@@ -7,39 +7,37 @@ from rexmex.metricset import ClassificationMetricSet, RatingMetricSet
 
 class TestMetricAggregation(unittest.TestCase):
 
-    def test_classification(self):
+    def setUp(self):
         reader = DatasetReader()
-        scores = reader.read_dataset()
+        self.scores = reader.read_dataset()
+
+    def test_classification(self):
         metric_set = ClassificationMetricSet()
         score_card = ScoreCard(metric_set)
 
-        performance_metrics = score_card.generate_report(scores, groupping=["source_group"])
+        performance_metrics = score_card.generate_report(self.scores, groupping=["source_group"])
         assert performance_metrics.shape == (5, 11)
 
-        performance_metrics = score_card.generate_report(scores, groupping=["source_group", "target_group"])
+        performance_metrics = score_card.generate_report(self.scores, groupping=["source_group", "target_group"])
         assert performance_metrics.shape == (20, 11)
 
     def test_regression(self):
-        reader = DatasetReader()
-        scores = reader.read_dataset()
         metric_set = RatingMetricSet()
         metric_set.normalize_metrics()
         score_card = ScoreCard(metric_set)
 
-        performance_metrics = score_card.generate_report(scores, groupping=["source_group"])
+        performance_metrics = score_card.generate_report(self.scores, groupping=["source_group"])
         assert performance_metrics.shape == (5, 7)
 
-        performance_metrics = score_card.generate_report(scores, groupping=["source_group", "target_group"])
+        performance_metrics = score_card.generate_report(self.scores, groupping=["source_group", "target_group"])
         assert performance_metrics.shape == (20, 7)
 
     def test_addition(self):
-        reader = DatasetReader()
-        scores = reader.read_dataset()
         metric_set = RatingMetricSet() + ClassificationMetricSet()
         score_card = ScoreCard(metric_set)
 
-        performance_metrics = score_card.generate_report(scores, groupping=["source_group"])
+        performance_metrics = score_card.generate_report(self.scores, groupping=["source_group"])
         assert performance_metrics.shape == (5, 18)
 
-        performance_metrics = score_card.generate_report(scores, groupping=["source_group", "target_group"])
+        performance_metrics = score_card.generate_report(self.scores, groupping=["source_group", "target_group"])
         assert performance_metrics.shape == (20, 18)

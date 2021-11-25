7cd5f4503cbcd13b87724917a71693ffc035d96b
Rozemberczki <kmdb028@astrazeneca.net>
2021-11-25 11:25:19 +0000

    Docs

--- rexmex/metrics/classification.py
+++ rexmex/metrics/classification.py
@@ -44,35 +44,9 @@ def pr_auc_score(y_true: np.array, y_scores: np.array) -> float:
     Returns:
         pr_auc (float): The value of the precision-recall area under the curve.
     """
-    precision, recall, thresholds = precision_recall_curve(y_true, y_scores)
-    pr_auc = auc(recall, precision)
+    precision, recall, thresholds = sklearm.metrics.precision_recall_curve(y_true, y_scores)
+    pr_auc = sklearm.metrics.auc(recall, precision)
     return pr_auc
-
-def root_mean_squared_error(y_true: np.array, y_scores: np.array) -> float:
-    """
-    Calculate the root mean squared error (RMSE) for a ground-truth prediction vector pair.
-
-    Args:
-        y_true (array-like): An N x 1 array of ground truth values.
-        y_scores (array-like):  An N x 1 array of predicted values.
-    Returns:
-        rmse (float): The value of the root mean squared error.
-    """
-    rmse = mean_squared_error(y_true, y_scores)**0.5
-    return rmse
-
-def symmetric_mean_absolute_percentage_error(y_true: np.array, y_scores: np.array) -> float:
-    """
-    Calculate the symmetric mean absolute percentage error (SMAPE) for a ground-truth prediction vector pair.
-
-    Args:
-        y_true (array-like): An N x 1 array of ground truth values.
-        y_scores (array-like):  An N x 1 array of predicted values.
-    Returns:
-        smape (float): The value of the symmetric mean absolute percentage error .
-    """
-    smape = 100*np.mean(np.abs(y_scores - y_true)/((np.abs(y_scores)+np.abs(y_true))/2))
-    return smape
 
 def condition_positive(y_true: np.array) -> float:
     """

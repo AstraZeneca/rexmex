ad2807ee6d2982fa641ebc2f4934c5dcd0dddfee
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-11-29 13:46:33 -0500

    Update dataset.py

--- rexmex/dataset.py
+++ rexmex/dataset.py
@@ -14,9 +14,6 @@ class DatasetReader(object):
 
     def __init__(self):
 
-
-
-
         self.base_url = (
             "https://raw.githubusercontent.com/AstraZeneca/rexmex/main/dataset/"
         )

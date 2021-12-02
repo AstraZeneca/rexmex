3b74949ff52e91c5f0a33353ec5ba0dc4a8472b9
Rozemberczki <kmdb028@astrazeneca.net>
2021-12-02 14:27:32 +0000

    Scorecard test restructuring

--- tests/unit/test_scorecard.py
+++ tests/unit/test_scorecard.py
@@ -8,18 +8,19 @@ from rexmex.scorecard import ScoreCard
 
 
 class TestMetricSet(unittest.TestCase):
+
+    def setUp(self):
+        self.metric_set = ClassificationMetricSet()
+        self.score_card = ScoreCard(self.metric_set)
+
     def test_representation(self):
-        metric_set = ClassificationMetricSet()
-        score_card = ScoreCard(metric_set)
-        assert repr(score_card) == "ScoreCard()"
+        assert repr(self.score_card) == "ScoreCard()"
 
     def test_printing(self):
-        metric_set = ClassificationMetricSet()
-        metric_set.filter_metrics(["roc_auc", "pr_auc"])
-        score_card = ScoreCard(metric_set)
+        self.score_card.metric_set.filter_metrics(["roc_auc", "pr_auc"])
         captured = StringIO()
         sys.stdout = captured
-        score_card.print_metrics()
+        self.score_card.print_metrics()
         sys.stdout = sys.__stdout__
         out = captured.getvalue().strip("\n")
         assert out == str({"roc_auc", "pr_auc"})

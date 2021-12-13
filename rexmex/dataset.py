ddd4a7694c95f8a62b1c890b690fa0d598cc159f
Rozemberczki <kmdb028@astrazeneca.net>
2021-12-13 13:44:37 +0000

    Filter docstring

--- rexmex/dataset.py
+++ rexmex/dataset.py
@@ -5,20 +5,18 @@ from six.moves import urllib
 
 
 class DatasetReader(object):
-    r"""Class to read synthetic test datasets.
-    Args:
-        dataset (str): Dataset of interest, one of:
-            (:obj:`"erdos_renyi_example"`). Default is 'erdos_renyi_example'.
-    Returns:
-        data (pd.DataFrame): The example dataset for testing the library.
-    """
+    """Class to read synthetic test datasets."""
 
     def __init__(self):
         self.base_url = "https://raw.githubusercontent.com/AstraZeneca/rexmex/main/dataset/"
 
     def read_dataset(self, dataset: str = "erdos_renyi_example"):
-        """
-        Reading the dataset from the web.
+        """Method to read the dataset.
+        Args:
+            dataset (str): Dataset of interest, one of:
+                (:obj:`"erdos_renyi_example"`). Default is 'erdos_renyi_example'.
+        Returns:
+            data (pd.DataFrame): The example dataset for testing the library.
         """
         assert dataset in ["erdos_renyi_example"], "Wrong dataset."
         path = self.base_url + dataset + ".csv"
